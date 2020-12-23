7186ab1bda4792761030d59590d55da2457ac738
Adriannom <64039731+Adriannom@users.noreply.github.com>
2020-12-23 21:38:31 +0000

    Refactor and add single table reroll.

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,6 @@
-export const TEMPLATE_PATHS = {
+export const templatePaths = {
     main: "modules/table-ninja/templates/main.hbs",
     group: "modules/table-ninja/templates/partials/group.hbs",
-    rolls: "modules/table-ninja/templates/partials/rolls.hbs"
+    rolls: "modules/table-ninja/templates/partials/table.hbs"
 }
+export const numberOfRolls = 50;
--- scripts/module.js
+++ scripts/module.js
@@ -1,31 +1,29 @@
-import {TEMPLATE_PATHS} from "./config.js";
+import * as config from "./config.js";
 
 class TableNinja extends Application {
 
     id = "table-ninja-prime";
     title = "Table Ninja";
-    template = TEMPLATE_PATHS.main;
+    template = config.templatePaths.main;
     tableNinjaFolderName = "Table Ninja";
-    numberToDraw = 30;
 
     static get defaultOptions() {
         const options = super.defaultOptions
         options.width = 400;
-        options.height= 600;
+        options.height = 600;
         options.resizable = true;
         return options;
     }
 
     getData() {
         return {
-            data: this.data,
-            partials: TEMPLATE_PATHS
+            data: this.data
         }
     }
 
     constructor(options = {}) {
         super(options);
-        this.tableNinjaFolder = game.folders.filter(x => x.data.type == "RollTable").find(b => b.name === this.tableNinjaFolderName);
+        this.data = game.folders.filter(x => x.data.type == "RollTable").find(b => b.name === this.tableNinjaFolderName);
         this.refresh();
         this.tabs = new Tabs({navSelector: ".tabs", contentSelector: ".content", initial: "tab1"});
         //this.tabs.bind(html);
@@ -44,67 +42,67 @@ class TableNinja extends Application {
 
     }
 
-    refresh() {
-
-        // Retrieve everything from the table folder "Table Ninja".
-        this.data = this.fetchRolls(this.tableNinjaFolder);
-    
-        this.render();
-
+    async refresh() {
+        this.initData(this.data).then((promise) => {
+            this.data = promise;
+            this.render();
+        });
     }
 
-    fetchRolls(requestedFolder) {
+    async initData(folder = null) {
 
-        const tables = requestedFolder.content;
-        const folders = requestedFolder.children;
-        let output = [];
-
-        // Roll many times on each table.
+        let tables = folder.content;
+        let subFolders = folder.children;
+        folder.isFolder = true;
+        
+        // Erase child entities and combine folders and tables.
+        folder.childEntities = [];
+        for (let i = 0; i < subFolders.length; i++) {
+            let subFolder = subFolders[i];
+            this.initData(subFolder).then((promise) => {
+                folder.childEntities.push(promise);
+            })
+        }
         for (let i = 0; i < tables.length; i++) {
             let table = tables[i];
-            this.rollMany(table.name).then((promise) => {
-                output.push({
-                    id: table.id,
-                    name: table.name,
-                    rolls: promise.results
-                });
+            if (typeof table.ninjaRoll === "undefined") {
+                table.ninjaRoll = async function () {
+                    return this.drawMany(config.numberOfRolls, {displayChat: false}).then((promise) => {
+                        this.rolls = promise.results;
+                        return this;
+                    });
+                }
+            }
+            table.ninjaRoll().then((promise) => {
+                folder.childEntities.push(promise);
             });
         }
 
-        // Recurse through subfolders.
-        for (let i = 0; i < folders.length; i++) {
-            let folder = Object.assign({}, folders[i]);
-            folder.childEntities = this.fetchRolls(folder);
-            folder.name = folders[i].name;
-            folder.folder = true;
-            output.push(folder);
-        }
-
-        return output;
-
+        return folder;
+    
     }
 
-    rollMany(rollTableName) {
-        const rollTable = game.tables.entities.find(b => b.name === rollTableName);
-        if (rollTable.data.results.length > 0) {
-            return rollTable.drawMany(this.numberToDraw, {displayChat: false});
-        }
+    rollOneTable(id) {
+        let table = game.tables.find(b => b.id === id);
+        table.ninjaRoll().then(() => {
+            this.render();
+        });
     }
 
 }
 
-// Add a button to the scene controls for quick access. Using @errational's hack to avoid adding a layer. May break at some point.
 Hooks.on("renderSceneControls", async (app, html, data) => {
+
+    // Add a button to the scene controls for quick access. Using @errational's hack to avoid adding a layer. May break at some point.
     const tableNinjaButtonHtml = await renderTemplate(`modules/table-ninja/templates/button.hbs`);
     html.append(tableNinjaButtonHtml);
     const tableNinjaButton = html.find("li[data-control='table-ninja']");
     tableNinjaButton.on("click", event => ui.tableNinja.toggleOpen());
+
 });
 
 Hooks.once('ready', async function() {
-    if (ui.tableNinja === undefined) {
-        ui.tableNinja = new TableNinja();
-    }
+    ui.tableNinja = new TableNinja();
 });
 
 Hooks.once("init", function () {
@@ -112,7 +110,7 @@ Hooks.once("init", function () {
 });
 
 async function preloadHandlebarsTemplates() {
-    return loadTemplates(Object.values(TEMPLATE_PATHS));
+    return loadTemplates(Object.values(config.templatePaths));
 };
 
 Handlebars.registerHelper('table-ninja-truncate', function(s) {
